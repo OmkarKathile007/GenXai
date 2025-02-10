da44a72e4cba1c076233481bcd38e181610438d3
Omkar Kathile <omkarkathile007@gmail.com>
2025-02-10 14:34:00 +0530

    Change in home route

--- App/app/page.jsx
+++ App/app/page.jsx
@@ -345,7 +345,7 @@ export default function Home() {
           <div className="flex flex-col items-center justify-center space-y-4 text-center max-w-3xl mx-auto">
             <h2 className="text-3xl font-bold tracking-tighter text-primary-foreground sm:text-4xl md:text-5xl">Ready to Accelerate Your Career?</h2>
             <p className="mx-auto max-w-[600px] text-primary-foreground/80 md:text-xl">Join thousands of professionals who are advancing their careers with AI-powered Tools</p>
-            <Link href="/dashboard" passHref>
+            <Link href="/" passHref>
               <Button size="lg" variant="secondary" className="h-11 mt-5 animate-bounce">
                 Start Your Journey Today <ArrowRight className="ml-2 h-4 w-4" />
               </Button>
