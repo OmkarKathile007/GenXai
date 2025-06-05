d5c8aba825fd94f1cccb53663eee3b5dda311b51
Omkar Kathile <omkarkathile420@gmail.com>
2025-06-05 23:46:44 +0530

    ok1

--- App/app/layout.js
+++ App/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
           >
             {/* {headers} */}
             <Header/>
-            <main className="min-h-screen">{children}</main>
+            <main className="min-h-screen">{children} <Analytics /></main>
             <Toaster richColors/>
             {/* <footer className="text-white bg-muted/50 py-12">
                 <div className="container mx-auto  px-4 text-center text-gray-200">
